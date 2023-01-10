dc16e05789c7406f96f1bc45a9e83b6336f3ece5
Cody Kilpatrick <2489375+Codykilpatrick@users.noreply.github.com>
2023-01-10 09:08:32 -0500

    add default value for airport

--- controllers/flights.js
+++ controllers/flights.js
@@ -8,6 +8,9 @@ function newFlight(req, res){
 
 function create(req, res){
   console.log(req.body);
+  for (const key in req.body) {
+    if (req.body[key] === '') delete req.body[key]
+	}
   Flight.create(req.body)
   .then(flight => {
     res.redirect('/flights')
